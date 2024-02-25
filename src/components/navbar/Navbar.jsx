8daee9ea26e537640fda69711faa39f233631d7a
Rudra Patel <rudra.190410116095@gmail.com>
2024-02-25 14:52:55 -0500

    categorized skills

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,57 +10,59 @@ function Navbar() {
     }, [activePath])
     return (
         <div
-            className='h-16 w-full py-2 px-8 bg-transparent flex items-center justify-between text-gray-300 text-lg leading-none gap-x-8'
+            className='h-16 w-full py-2 px-8 bg-transparent flex items-center justify-between text-lg leading-none gap-x-8'
         >
             <Link
                 to={'/'}
                 className={classNames(
-                    'tracking-wide py-1.5 px-2.5 rounded',
-                    activePath == '/' ? 'bg-[#123456bb] shadow shadow-gray-600' : 'bg-transparent'
+                    'tracking-wider py-1.5 px-2.5 rounded font-semibold',
+                    activePath == '/'
+                        ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                        : 'bg-transparent text-gray-200'
                 )}
                 onClick={() => setActivePath('/')}
             >
                 Rudra
             </Link>
             <div className='flex items-center gap-x-2.5'>
-                {/* <Link
-                    to={'/'}
-                    className={classNames(
-                        '',
-                        activePath == '/' ? 'underline underline-offset-4' : '')
-                    }
-                    onClick={() => setActivePath('/')}
-                >Home</Link> */}
                 <Link
                     to={'/projects'}
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
-                        activePath == '/projects' ? 'bg-[#123456bb] shadow shadow-gray-600' : 'bg-transparent')
-                    }
+                        activePath == '/projects'
+                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            : 'bg-transparent text-gray-200'
+                    )}
                     onClick={() => setActivePath('/projects')}
                 >Projects</Link>
                 <Link
                     to={'/experience'}
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
-                        activePath == '/experience' ? 'bg-[#123456bb] shadow shadow-gray-600' : 'bg-transparent')
-                    }
+                        activePath == '/experience'
+                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            : 'bg-transparent text-gray-200'
+                    )}
                     onClick={() => setActivePath('/experience')}
                 >Experience</Link>
                 <Link
                     to={'/education'}
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
-                        activePath == '/education' ? 'bg-[#123456bb] shadow shadow-gray-600' : 'bg-transparent')
-                    }
+                        activePath == '/education'
+                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            : 'bg-transparent text-gray-200'
+                    )}
                     onClick={() => setActivePath('/education')}
                 >Education</Link>
                 <Link
                     to={'/skills'}
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
-                        activePath == '/skills' ? 'bg-[#123456bb] shadow shadow-gray-600' : 'bg-transparent')
-                    }
+                        activePath == '/skills'
+                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            : 'bg-transparent text-gray-200'
+                    )}
                     onClick={() => setActivePath('/skills')}
                 >Skills</Link>
             </div>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,18 +3,69 @@ import { techSkills } from '../../data'
 
 function Skills() {
     return (
-        <div className='h-[calc(100%-64px)] w-full p-4 flex justify-center text-white'>
-            <div
-                className='grid w-[60%] h-full overflow-y-auto grid-cols-2 gap-3'
-            >
-                {techSkills?.map((ts, index) => (
-                    <div
-                        key={index}
-                        className='w-[100%] py-3 px-2 rounded-md bg-[#12345695] shadow shadow-[#12345695] flex items-center justify-center gap-x-2 text-lg leading-none'
-                    >
-                        <ts.Component className='text-3xl leading-none' />{ts.sname}
-                    </div>
-                ))}
+        <div
+            className='h-[calc(100%-64px)] w-full p-4 flex flex-col items-center overflow-y-auto text-white gap-y-6'
+            style={{ scrollbarWidth: 'none' }}
+        >
+            <div className='w-[60%] flex flex-col gap-y-3'>
+                <div className='text-2xl leading-none px-1'>Languages</div>
+                <div
+                    className='grid w-[100%] h-full grid-cols-2 gap-3'
+                >
+                    {techSkills?.filter(x => x.category == 'lang')?.map((ts, index) => (
+                        <div
+                            key={index}
+                            className='w-[100%] py-3 px-2 rounded-md bg-[#12345695] shadow shadow-[#12345695] flex items-center justify-center gap-x-2 text-lg leading-none'
+                        >
+                            <ts.Component className='text-3xl leading-none' />{ts.sname}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='w-[60%] flex flex-col gap-y-3'>
+                <div className='text-2xl leading-none px-1'>Libraries & Frameworks</div>
+                <div
+                    className='grid w-[100%] h-full grid-cols-2 gap-3'
+                >
+                    {techSkills?.filter(x => x.category == 'l&f')?.map((ts, index) => (
+                        <div
+                            key={index}
+                            className='w-[100%] py-3 px-2 rounded-md bg-[#12345695] shadow shadow-[#12345695] flex items-center justify-center gap-x-2 text-lg leading-none'
+                        >
+                            <ts.Component className='text-3xl leading-none' />{ts.sname}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='w-[60%] flex flex-col gap-y-3'>
+                <div className='text-2xl leading-none px-1'>Databases</div>
+                <div
+                    className='grid w-[100%] h-full grid-cols-2 gap-3'
+                >
+                    {techSkills?.filter(x => x.category == 'db')?.map((ts, index) => (
+                        <div
+                            key={index}
+                            className='w-[100%] py-3 px-2 rounded-md bg-[#12345695] shadow shadow-[#12345695] flex items-center justify-center gap-x-2 text-lg leading-none'
+                        >
+                            <ts.Component className='text-3xl leading-none' />{ts.sname}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='w-[60%] flex flex-col gap-y-3'>
+                <div className='text-2xl leading-none px-1'>Version Control</div>
+                <div
+                    className='grid w-[100%] h-full grid-cols-2 gap-3'
+                >
+                    {techSkills?.filter(x => x.category == 'vcs')?.map((ts, index) => (
+                        <div
+                            key={index}
+                            className='w-[100%] py-3 px-2 rounded-md bg-[#12345695] shadow shadow-[#12345695] flex items-center justify-center gap-x-2 text-lg leading-none'
+                        >
+                            <ts.Component className='text-3xl leading-none' />{ts.sname}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )

d4a01a4557738a2f91267448789053b9b9dee36d
Rudra Patel <rudra.190410116095@gmail.com>
2024-02-28 20:17:17 -0500

    added mobile sidebar at screen:md

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,90 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from 'react-icons/rx';
 
 function Navbar() {
     const [activePath, setActivePath] = useState("/");
+    const [openSidebar, setOpenSidebar] = useState(false);
+
     useEffect(() => {
         const pathname = window.location.pathname;
         console.log(pathname)
     }, [activePath])
+
+    const mobileSidebar = (
+        <div
+            className='absolute top-0 left-0 bottom-0 w-3/5 bg-gray-300 text-gray-800 px-8 py-14 flex flex-col gap-y-4 items-center'
+        >
+            <RxCross2
+                className='absolute top-3 right-3 text-3xl leading-none cursor-pointer'
+                onClick={() => setOpenSidebar(false)}
+            />
+            <div className='font-semibold text-2xl font-mono leading-none mb-3'>Rudra Patel</div>
+            <Link
+                to={'/'}
+                className=''
+            >
+                Home
+            </Link>
+            <Link
+                to={'/projects'}
+                className=''
+            >
+                Projects
+            </Link>
+            <Link
+                to={'/experience'}
+                className=''
+            >
+                Experience
+            </Link>
+            <Link
+                to={'/education'}
+                className=''
+            >
+                Education
+            </Link>
+            <Link
+                to={'/skills'}
+                className=''
+            >
+                Skills
+            </Link>
+        </div>
+    );
+
     return (
         <div
             className='h-16 w-full py-2 px-8 bg-transparent flex items-center justify-between text-lg leading-none gap-x-8'
         >
+            {openSidebar ? mobileSidebar : null}
+            {openSidebar
+                ? null
+                : <GiHamburgerMenu
+                    className='md:hidden block text-2xl leading-none text-gray-200 cursor-pointer'
+                    onClick={() => setOpenSidebar(true)}
+                />}
             <Link
                 to={'/'}
                 className={classNames(
-                    'tracking-wider py-1.5 px-2.5 rounded font-semibold',
+                    'tracking-wider py-1.5 px-2.5 rounded font-semibold md:block hidden',
                     activePath == '/'
-                        ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                        ? 'bg-[#deb887] text-gray-800 shadow shadow-gray-600'
                         : 'bg-transparent text-gray-200'
                 )}
                 onClick={() => setActivePath('/')}
             >
                 Rudra
             </Link>
-            <div className='flex items-center gap-x-2.5'>
+            <div className='hidden md:flex md:items-center gap-x-2.5'>
                 <Link
                     to={'/projects'}
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
                         activePath == '/projects'
-                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            ? 'bg-[#deb887] text-gray-800 shadow shadow-gray-600'
                             : 'bg-transparent text-gray-200'
                     )}
                     onClick={() => setActivePath('/projects')}
@@ -40,7 +94,7 @@ function Navbar() {
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
                         activePath == '/experience'
-                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            ? 'bg-[#deb887] text-gray-800 shadow shadow-gray-600'
                             : 'bg-transparent text-gray-200'
                     )}
                     onClick={() => setActivePath('/experience')}
@@ -50,7 +104,7 @@ function Navbar() {
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
                         activePath == '/education'
-                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            ? 'bg-[#deb887] text-gray-800 shadow shadow-gray-600'
                             : 'bg-transparent text-gray-200'
                     )}
                     onClick={() => setActivePath('/education')}
@@ -60,7 +114,7 @@ function Navbar() {
                     className={classNames(
                         'py-1.5 px-2.5 rounded tracking-wide',
                         activePath == '/skills'
-                            ? 'bg-[#deb887] text-gray-700 shadow shadow-gray-600'
+                            ? 'bg-[#deb887] text-gray-800 shadow shadow-gray-600'
                             : 'bg-transparent text-gray-200'
                     )}
                     onClick={() => setActivePath('/skills')}

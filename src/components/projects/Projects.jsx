7e8d04bf2c6bd73c0b7e63a18788730a56807921
Rudra Patel <rudra.190410116095@gmail.com>
2024-05-30 11:14:10 -0400

    Updated projects mobile UI

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -93,7 +93,7 @@ function Projects() {
                             </div>
                         </div>
                         <div
-                            className='text-xs text-gray-100 mx-1 flex items-center gap-x-2 my-1 py-1 px-2 rounded bg-blue-600 w-fit leading-none tracking-wide'
+                            className='text-xs text-gray-100 mx-1 flex items-center gap-x-2 my-1 py-1 px-2 rounded bg-green-500 w-fit leading-none tracking-wide'
                         >
                             {project.category} Project
                         </div>
